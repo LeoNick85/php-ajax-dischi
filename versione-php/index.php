f78d9f6228e65943c81a31fea976ccf173831fa4
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-06-17 16:32:35 +0200

    PHP Version 1.0

--- versione-php/index.php
+++ versione-php/index.php
@@ -11,12 +11,27 @@
     </head>
     <body>
         <header>
-            <h1>Lista dischi</h1>
+            <img src="../dist/img/logo.png" alt="Logo">
+            <h1>Album Showroom</h1>
         </header>
         <main>
-            <?php
-            var_dump($dischi);
-            ?>
+            <div class="container">
+                <?php
+                foreach ($dischi as $key => $value) {
+                    ?> <div class="album-card"> <?php
+                    ?> <div class="album-card-up"> <?php
+                        echo '<img src="' . $dischi[$key]["poster"] . '" alt="Poster">';
+                        echo '<h2>' . $dischi[$key]["title"] . '</h2>';
+                        echo '<h3>' . $dischi[$key]["author"] . '</h3>';
+                        ?></div>
+                        <div class="album-card-down"> <?php
+                        echo '<h4>' . $dischi[$key]["genre"] . '</h4>';
+                        echo '<h5>' . $dischi[$key]["year"] . '</h5>';
+                        ?> </div>
+                    </div> <?php
+                }
+                ?>
+            </div>
         </main>
     </body>
 </html>
